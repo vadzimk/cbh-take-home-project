2ebb47500b947f6c7520747b772ec376b4364f51
Vadzim <vadzimk@hotmail.com>
2022-11-02 06:29:38 -0700

    done

--- dpk.js
+++ dpk.js
@@ -1,28 +1,44 @@
 const crypto = require("crypto");
 
+const hexDigest = (obj) => {
+  return crypto.createHash("sha3-512").update(obj).digest("hex");
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
+  if (!event.partitionKey) {
+    return hexDigest(JSON.stringify(event))
+  }
+
+  if (typeof event.partitionKey === "string") {
+    if (event.partitionKey.length <= MAX_PARTITION_KEY_LENGTH) {
+      return event.partitionKey
+    } else {
+      return hexDigest(event.partitionKey)
     }
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const candidate = JSON.stringify(event.partitionKey)
+    if( candidate.length <= MAX_PARTITION_KEY_LENGTH){
+      return candidate
+    } else {
+      return hexDigest(candidate)
+    }
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
 };
+
+/*
+* Explanation:
+* Partitioned input domain into blocks.
+* For each of the blocks, if the function returns a trivial value it does so immediately.
+* If input types determine return values, grouped them into common control flow branches.
+* Factored out digest computation into a function.
+* Reduced the scope of the helper variable `candidate`.
+*
+* */
--- dpk.test.js
+++ dpk.test.js
@@ -2,12 +2,13 @@ const {deterministicPartitionKey} = require("./dpk");
 const crypto = require('crypto');
 
 describe("deterministicPartitionKey", () => {
-  const stringifyAndHash = (obj) => {
-    const data = JSON.stringify(obj);
-    return crypto.createHash("sha3-512").update(data).digest("hex");
-  }
-  const makeHash = (obj) => {
+
+  const hexDigest = (obj) => {
     return crypto.createHash("sha3-512").update(obj).digest("hex");
+  }
+  const stringifyAndDigest = (obj) => {
+    const data = JSON.stringify(obj);
+    return hexDigest(data)
   }
 
   it("Returns the literal '0' when given no input", () => {
@@ -15,30 +16,11 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 
-  it("Returns hash of stringified event when event.partitionKey is a string of length 0", () => {
-    const event = {
-      partitionKey: ""
-    }
-    expect(deterministicPartitionKey(event)).toBe(stringifyAndHash(event))
-  })
-
   it("Returns hash of stringified event when event.partitionKey is falsy", () => {
     const event = {
-      partitionKey: false
+      partitionKey: false,
     }
-    expect(deterministicPartitionKey(event)).toBe(stringifyAndHash(event))
-  })
-
-  it("Returns hash of stringified event when event is empty object", () => {
-    const event = {}
-    expect(deterministicPartitionKey(event)).toBe(stringifyAndHash(event))
-  })
-
-  it("Returns hash of stringified event when event doesn't contain partitionKey field", () => {
-    const event = {
-      other: [1, 2]
-    }
-    expect(deterministicPartitionKey(event)).toBe(stringifyAndHash(event))
+    expect(deterministicPartitionKey(event)).toBe(stringifyAndDigest(event))
   })
 
   it("Returns event.partitionKey when event.partitionKey is a string of length<256", () => {
@@ -59,7 +41,7 @@ describe("deterministicPartitionKey", () => {
     const event = {
       partitionKey: "s".repeat(257)
     }
-    expect(deterministicPartitionKey(event)).toBe(makeHash(event.partitionKey))
+    expect(deterministicPartitionKey(event)).toBe(hexDigest(event.partitionKey))
   })
 
   it("Returns strignified event.partitionKey when event.partitionKey is not a string and truthy and its length<256", () => {
@@ -74,18 +56,14 @@ describe("deterministicPartitionKey", () => {
     const event = {
       partitionKey: [str]
     }
-    // console.log(JSON.stringify(event.partitionKey).length)
     expect(deterministicPartitionKey(event)).toBe(JSON.stringify(event.partitionKey))
   })
-/**
- * unnecessary branch
- * */
-  it("Returns strignified event.partitionKey when event.partitionKey is not a string and truthy and its length>256", () => {
+
+  it("Returns hash of strignified event.partitionKey when event.partitionKey is not a string and truthy and its length>256", () => {
     const str = "s".repeat(253)
     const event = {
       partitionKey: [str]
     }
-    // console.log(JSON.stringify(event.partitionKey).length)
-    expect(deterministicPartitionKey(event)).toBe(stringifyAndHash(event.partitionKey))
+    expect(deterministicPartitionKey(event)).toBe(stringifyAndDigest(event.partitionKey))
   })
 });
